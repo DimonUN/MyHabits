faeb3adef960b8b0de276c4cb421d8e50838e9d5
DimonUN <nikonorov_photo@mail.ru>
2022-02-22 10:39:50 +0300

    Создан проект. Добавлен файл HabitsStore.swift.

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Дмитрий Никоноров on 21.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
